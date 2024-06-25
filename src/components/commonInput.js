f7df5d122d459fe14d1c24cd58ba9fb4cbca7153
Rahul Tile <tilerahul8068@gmail.com>
2024-06-25 14:26:25 +0530

    Level 1 Completed

--- src/components/commonInput.js
+++ src/components/commonInput.js
@@ -8,28 +8,68 @@ const InputHandler = ({ onSubmit, editMode = false }) => {
     e.preventDefault();
     if (!name || !email) return;
     onSubmit({ name, email });
+    setName("");
+    setEmail("");
+    alert("User Added Successfully !!!")
   };
 
   return (
-    <div className="header-box">
-      <input
-        type="text"
-        placeholder="Name"
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <button type="primary">
-        {!!editMode ? "Edit user" : "Add user"}
-      </button>
-    </div>
+    <section className="pt-0">
+      <div className="flex justify-center h-auto">
+        <div className="w-full max-w-md bg-white rounded-lg shadow-lg mt-9">
+          <div className="p-6 space-y-4 md:space-y-6">
+
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              User Details Form
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">
+                  Your Name
+                </label>
+                <input
+                  type="name"
+                  name="name"
+                  id="name"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5 "
+                  placeholder="Enter your name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
+                  Email
+                </label>
+                <div className='flex items-center'>
+                  <input
+                    type='email'
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                    placeholder="Enter Your Email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 block w-full p-2.5"
+                    required
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                name='submit'
+                className="w-full text-white bg-sky-400 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                {!!editMode ? "Edit user" : "Add user"}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/simpleTable.js
+++ src/components/simpleTable.js
@@ -1,24 +1,63 @@
 import { React } from "react";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const SimpleTable = ({ dataSource }) => {
   return (
-    <div>
-      {dataSource.length ? (
-        <>
-          {dataSource.map((item, index) => {
-            return (
-              <div key={index}>
-                <div>Id: {item.id}</div>
-                <div>Name: {item.name}</div>
-                <div>Email: {item.email}</div>
-              </div>
-            );
-          })}
-        </>
-      ) : (
-        "No user data"
-      )}
-    </div>
+    <>
+      <div>
+        {dataSource.length ? ( 
+            <div class="w-[70vw] m-auto py-5 my-5 relative overflow-x-auto sm:rounded-lg">
+              <table class="w-full text-sm text-left  shadow-lg  rtl:text-right text-gray-500">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+                  <tr>
+                    <th scope="col" class="px-6 py-3">
+                      ID
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                      Email
+                    </th>
+                    <th scope="col" class="text-center px-6 py-3">
+                      Operation
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dataSource.map((user, index) => (
+                    <tr key={index} class="bg-white border-b hover:bg-gray-50">
+                      <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {user.id}
+                      </th>
+                      <td class="px-6 py-4">
+                        {user.name}
+                      </td>
+                      <td class="px-6 py-4">
+                        {user.email}
+                      </td>
+                      <td class="flex justify-around px-6 py-4">
+                        <FaEdit className="text-blue-500 cursor-pointer" />
+                        <MdDelete size={18} color="red" className="cursor-pointer" />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+        ) : (
+          <div>
+            <h3 className="text-center py-20 text-lg font-bold">No User Found</h3>
+          </div>
+        )}
+      </div>
+
+
+
+
+
+    </>
   );
 };
 
